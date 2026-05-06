9e3f79227dea02af5197f19350f0c24122740dcb
Sara <sala0002@stud.kea.dk>
2026-05-06 11:04:54 +0200

    404

---
+++ pages/page-404.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Side ikke fundet – MessBolig";
+require_once __DIR__."/../components/_header.php";
+?>
+
+<main class="page-form">
+    <div class="form-box">
+        <h1>404</h1>
+        <p>Siden blev ikke fundet.</p>
+        <a href="/">Gå til forsiden</a>
+    </div>
+</main>
+
+<?php require_once __DIR__."/../components/_footer.php"; ?>
