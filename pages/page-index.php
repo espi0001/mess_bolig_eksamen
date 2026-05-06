bb99185860664dad3b321cd3b9d1f146c8474c0c
Sara <sala0002@stud.kea.dk>
2026-05-06 10:34:04 +0200

    map is updated to the system

--- pages/page-index.php
+++ pages/page-index.php
@@ -1,17 +1,30 @@
 <?php
-$title = "Home";
-$active = "index";
-
-require_once __DIR__.'/../components/_header.php';
-
-$user_input = '<script>alert()</script>';
-out($user_input);
-
+$title = "Messbolig";
+require_once __DIR__."/../components/_header.php";
 ?>
 
+<main>
+    <div id="map"></div>
+    <aside id="aside">
+        <p>Klik på en bolig på kortet for at se detaljer</p>
+    </aside>
+</main>
 
-<?php 
-require_once __DIR__.'/../components/_footer.php';
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+<script>
+    const map = L.map('map').setView([55.6761, 12.5683], 7);
 
-?>
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; OpenStreetMap &copy; CARTO',
+        subdomains: 'abcd',
+        maxZoom: 20
+    }).addTo(map);
 
+    const markers = L.layerGroup().addTo(map);
+    
+window.addEventListener('load', () => {
+    document.querySelector('[mix-get="api-get-items"]').click();
+});
+</script>
+
+<?php require_once __DIR__."/_footer.php"; ?>
--- static/app.js
+++ static/app.js
@@ -1,73 +1,17 @@
-function clear_markers(data){
-    console.log(data)
-    const urlParams = new URLSearchParams(window.location.search)
-    // urlParams.set('beds', '2')
-    // urlParams.set('baths', '1')
-    //window.history.replaceState({}, '', `?${urlParams.toString()}`)
+function load_markers(data_as_string) {
+  const items = JSON.parse(data_as_string);
+  markers.clearLayers();
 
-    // ?beds=2&baths=1
-
-
-    markers.clearLayers()
-    data = JSON.parse(data)
-    console.log(data.url)
-    data.url.forEach( prop => {
-        console.log(Object.keys(prop)[0]) // beds | baths
-        let key = Object.keys(prop)[0] // beds | baths
-        console.log(prop[key]) // 
-        let value = prop[key] // 2 | 1
-        urlParams.set(key, value)
-    } )
-    window.history.replaceState({}, '', `?${urlParams.toString()}`)
-
-    // items = data.items
-    // console.log(items)
-    data.items.forEach(item => {
-        if( item.type == "villa" ){
-            var marker = L.marker([item.lat, item.lon], {
-                icon: L.divIcon({
-                    className: '',
-                    html: `
-                        <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
-                        </button>
-                    `,
-                }),
-                item_pk: item.pk
-            });
-        }
-        else if( item.type == "condo" ){
-            var marker = L.marker([item.lat, item.lon], {
-                icon: L.divIcon({
-                    className: '',
-                    html: `
-                        <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
-                        </button>
-                    `,
-                }),
-                item_pk: item.pk
-            });
-        }
-        else{
-            var marker = L.marker([item.lat, item.lon], {
-                icon: L.divIcon({
-                    className: '',
-                    html: `
-                        <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
-                        </button>
-                    `,
-                }),
-                item_pk: item.pk
-            });
-        }
-        markers.addLayer(marker)
+  items.forEach((item) => {
+    const marker = L.marker([item.lat, item.lon], {
+      icon: L.divIcon({
+        className: "",
+        html: `<button class="marker" mix-get="api-get-item/${item.pk}"></button>`,
+      }),
+      pk: item.pk,
     });
-    map.addLayer(markers)
+    markers.addLayer(marker);
+  });
 
-
+  mix_convert();
 }
